a4238c4ef85d0bd3aa2013742e98e526bac9bf8c
stemming up <stemmingup@stemmings-MacBook-Pro.local>
2020-07-31 19:07:00 -0400

    hello world

---
+++ week1/day2.js
@@ -0,0 +1,19 @@
+day2.js 
+```jsx
+/*
+Declare two variable containing any 2 numbers.
+Print out the product, division and addition of the two numbers.
+Also print out True or False based on whether the two numbers are equal. 
+*/
+```
+let num1 = 12
+let num2 = 2
+
+let result = num1 + num2
+console.log('result',result)
+
+let num3 = 12
+let num4 = 2
+
+let product = num3 / num4
+console.log('product', product)
